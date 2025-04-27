45ddbeeb8ecb71e4e91da6e7e97889b76d496d3d
webdevred <148627186+webdevred@users.noreply.github.com>
2025-04-27 18:44:24 +0200

    starting using Sequence for NodeCursor

--- src/NodeCursor.hs
+++ src/NodeCursor.hs
@@ -8,6 +8,8 @@ module NodeCursor
   ) where
 
 import Data.List qualified as L (foldl')
+import Data.Sequence (Seq(..))
+import Data.Sequence qualified as Seq (empty, null)
 import Data.Text (Text)
 import Data.Text qualified as T
 import GHC.IsList (toList)
@@ -23,22 +25,23 @@ instance Show NodeBreadcrumb where
   show (ObjectIndexAndKey (_, k)) = "." <> T.unpack k
 
 newtype NodeCursor =
-  NodeCursor [NodeBreadcrumb]
+  NodeCursor (Seq NodeBreadcrumb)
 
 instance Show NodeCursor where
-  show (NodeCursor xs) = L.foldl' (flip $ (<>) . show) "" xs
+  show (NodeCursor (bs :|> b)) = show b <> show (NodeCursor bs)
+  show (NodeCursor Empty) = ""
 
 newCursor :: NodeCursor
-newCursor = NodeCursor []
+newCursor = NodeCursor Seq.empty
 
 type CursorFun a = NodeCursor -> Node -> a
 
 applyCrumb :: NodeBreadcrumb -> NodeCursor -> CursorFun a -> Node -> a
-applyCrumb b (NodeCursor bs) f = f (NodeCursor $ b : bs)
+applyCrumb b (NodeCursor bs) f = f (NodeCursor $ b :<| bs)
 
 applyObjCrumb :: Node -> NodeCursor -> CursorFun a -> Node -> a
-applyObjCrumb (String key) (NodeCursor ((ArrayIndex i):bs)) f =
-  f (NodeCursor $ ObjectIndexAndKey (i, key) : bs)
+applyObjCrumb (String key) (NodeCursor ((ArrayIndex i) :<| bs)) f =
+  f (NodeCursor $ ObjectIndexAndKey (i, key) :<| bs)
 applyObjCrumb key cursor _ = error $ unwords [errMsg, show key, show cursor]
   where
     errMsg =
@@ -53,11 +56,10 @@ compareSB (NP.ArrayIndex s) (ArrayIndex i) = s == i
 compareSB _ _ = False
 
 comparePathAndCursor :: NP.NodePath -> NodeCursor -> Bool
-comparePathAndCursor p (NodeCursor c) = sameBy compareSB (toList p) (reverse c)
+comparePathAndCursor p (NodeCursor c) = sameBy compareSB (toList p) c
 
-sameBy :: SelCrumbCompFun -> [NP.NodeSelector] -> [NodeBreadcrumb] -> Bool
+sameBy :: SelCrumbCompFun -> [NP.NodeSelector] -> Seq NodeBreadcrumb -> Bool
 sameBy f = go
   where
-    go (a:as) (b:bs) = f a b && go as bs
-    go [] [] = True
-    go _ _ = False
+    go (p:ps) (bs :|> b) = f p b && go ps bs
+    go ps bs = null ps && Seq.null bs
