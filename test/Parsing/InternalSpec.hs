2a02509adbb84ce4bc9113ba15d1d8bd3e7a5ac3
webdevred <148627186+webdevred@users.noreply.github.com>
2025-05-13 15:06:14 +0200

    add specs for the complex data structures

--- test/Parsing/InternalSpec.hs
+++ test/Parsing/InternalSpec.hs
@@ -2,6 +2,7 @@ module Parsing.InternalSpec
   ( spec
   ) where
 
+import Data.Vector (fromList)
 import Parsing.Internal
 import SpecHelper
 import Test.Hspec.Megaparsec
@@ -40,6 +41,26 @@ singlelineCommentSpec = do
     $ parse singlelineCommentParser "" "// test \n"
         `shouldParse` SinglelineComment "test"
 
+arraySpec :: Spec
+arraySpec = do
+  describe
+    "should parse [1,2,3] as Array (fromList [Number 1, Number 2,Number 3])"
+    . works
+    $ parse arrayParser "" "[1,2,3]"
+        `shouldParse` (Array (fromList [Number 1, Number 2, Number 3]))
+
+objectSpec :: Spec
+objectSpec = do
+  describe
+    "should parse {\"test\" : 1, \"test\" : 2} as Object (fromList [ObjectKey (String \"test\", Number 1), ObjectKey (String \"test2\", Number 2)])"
+    . works
+    $ parse objectParser "" "{\"test\" : 1, \"test2\" : 2}"
+        `shouldParse` (Object
+                         (fromList
+                            [ ObjectKey (String "test", Number 1)
+                            , ObjectKey (String "test2", Number 2)
+                            ]))
+
 spec :: Spec
 spec =
   sequence_
@@ -48,4 +69,6 @@ spec =
     , boolSpec
     , multilineCommentSpec
     , singlelineCommentSpec
+    , arraySpec
+    , objectSpec
     ]
