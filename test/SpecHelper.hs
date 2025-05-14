c6b05ea55fc9e9cb7034266d418e1d7b5decc0d9
webdevred <148627186+webdevred@users.noreply.github.com>
2025-05-14 20:49:14 +0200

    add type decl to applySpecOnInput

--- test/SpecHelper.hs
+++ test/SpecHelper.hs
@@ -8,6 +8,17 @@ module SpecHelper
 import Node
 import Test.Hspec
 
+type DescribeFun = (String -> String -> String)
+
+type SpecFun t1 t2 a = (t1 -> t2 -> a)
+
+applySpecOnInput ::
+     (Show t1, Show t2, Example a)
+  => DescribeFun
+  -> SpecFun t1 t2 a
+  -> t1
+  -> t2
+  -> SpecWith (Arg a)
 applySpecOnInput descFun spec input expResult =
   describe (descFun (show input) (show expResult)) . works
     $ spec input expResult
