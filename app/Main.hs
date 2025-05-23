f356beab50a13fccd77f9e5ed898f97b14522d01
webdevred <hej@example.org>
2025-05-23 21:20:07 +0200

    on failure show error message

--- app/Main.hs
+++ app/Main.hs
@@ -2,18 +2,23 @@ module Main
   ( main
   ) where
 
-import Data.Bifunctor (first)
+import Control.Exception
 import Core.NodeCursor (newCursor)
-import qualified Data.ByteString.Lazy as BL (ByteString,readFile, toStrict, writeFile)
+import qualified Data.ByteString.Lazy as BL
+  ( ByteString
+  , readFile
+  , toStrict
+  , writeFile
+  )
 import Data.Functor (($>))
 import qualified Data.List as L (uncons)
+import qualified Data.Text as T (append, pack)
+import Data.Text (Text)
 import qualified Data.Text.IO as TIO (putStrLn)
 import qualified Data.Text.Lazy as TL (fromStrict)
-import qualified Data.Text as T (pack)
-import Data.Text (Text)
 import Data.Text.Lazy.Encoding (encodeUtf8)
+import GHC.IO.Exception (IOErrorType(NoSuchThing), IOException(IOError))
 import System.Environment (getArgs)
-import Control.Exception
 import Transformation (transform)
 
 import Formatting
@@ -39,15 +44,27 @@ main = do
         Left err -> TIO.putStrLn err
     Nothing -> TIO.putStrLn "missing arg filename"
 
+ioErrorMsg ::
+     [IOErrorType]
+  -> Either IOException BL.ByteString
+  -> Either Text BL.ByteString
+ioErrorMsg noerrs (Left (IOError _ ioe_type _ ioe_desc _ filename)) =
+  if ioe_type `notElem` noerrs
+    then Left $ maybe "" appendColon filename `T.append` T.pack ioe_desc
+    else Right ""
+  where
+    appendColon f = T.pack f `T.append` ": "
+ioErrorMsg _ (Right valid) = Right valid
 
 tryReadFile :: FilePath -> IO (Either Text BL.ByteString)
 tryReadFile fp = do
-  possiblyContent <- try (BL.readFile fp) :: IO (Either IOException BL.ByteString)
-  pure $ first (T.pack . show) possiblyContent
+  possiblyContent <-
+    try (BL.readFile fp) :: IO (Either IOException BL.ByteString)
+  pure $ ioErrorMsg [NoSuchThing] possiblyContent
 
 readFormattingConfig :: IO RuleSet
 readFormattingConfig = do
   contents <- tryReadFile "rules.jbfl"
-  case contents >>= parseDSL . BL.toStrict  of
+  case contents >>= parseDSL . BL.toStrict of
     Right rs -> pure rs
     Left err -> TIO.putStrLn err $> newRuleSet
