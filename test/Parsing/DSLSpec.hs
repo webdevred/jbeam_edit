8bc659397194727aebb8736952c2a41b149070e2
webdevred <148627186+webdevred@users.noreply.github.com>
2025-06-08 18:53:19 +0200

    Added regression tests for JBFL properties

--- test/Parsing/DSLSpec.hs
+++ test/Parsing/DSLSpec.hs
@@ -1,37 +1,73 @@
-module Parsing.DSLSpec
-  ( spec
-  ) where
+module Parsing.DSLSpec (
+  spec,
+) where
 
+import Data.ByteString (ByteString)
 import Data.String (fromString)
+import Data.Void (Void)
 import Formatting.Rules
 import Parsing.Common.Helpers
 import Parsing.DSL
+import Parsing.ParsingTestHelpers
 import SpecHelper
 import Test.Hspec.Megaparsec
 import Text.Megaparsec
 
 intProperties :: [(String, (SomeKey, SomeProperty))]
 intProperties =
-  [ (" PadDecimals : 3;", (SomeKey PadDecimals, SomeProperty PadDecimals 3))
+  [ ("PadDecimals : 3;", (SomeKey PadDecimals, SomeProperty PadDecimals 3))
   , ("PadAmount : 8;", (SomeKey PadAmount, SomeProperty PadAmount 8))
   ]
 
 boolProperties :: [(String, (SomeKey, SomeProperty))]
 boolProperties =
-  [ ( "NoComplexNewLine : true;"
-    , (SomeKey NoComplexNewLine, SomeProperty NoComplexNewLine True))
+  [
+    ( "NoComplexNewLine : true;"
+    , (SomeKey NoComplexNewLine, SomeProperty NoComplexNewLine True)
+    )
   ]
+
+expLabels :: [String] -> ET ByteString
+expLabels = foldMap elabel
 
 keyPropertyPairSpecs :: [Spec]
 keyPropertyPairSpecs = map applyPropertySpec (boolProperties ++ intProperties)
   where
-    applyPropertySpec s@(_, (p, _)) = applyParserSpec keyPropertyPairParser s
+    applyPropertySpec = applyParserSpec keyPropertyPairParser
 
-applyParserSpec :: (Eq a, Show a) => Parser a -> (String, a) -> Spec
-applyParserSpec parser = uncurry $ applySpecOnInput descFun assertParsesTo
+invalidIntProperties :: [(String, ParseError ByteString Void)]
+invalidIntProperties =
+  [
+    ( "PadDecimals : true;"
+    , err 14 (utok (toWord8 't') <> expLabels ["integer", "white space"])
+    )
+  ,
+    ( "PadAmount : true;"
+    , err 12 (utok (toWord8 't') <> expLabels ["integer", "white space"])
+    )
+  ]
+
+invalidBoolProperties :: [(String, ParseError ByteString Void)]
+invalidBoolProperties =
+  [
+    ( "NoComplexNewLine : 3;"
+    , err 19 (utoks "3;" <> expLabels ["bool", "white space"])
+    )
+  ]
+
+invalidKeyPropertySpecs :: [Spec]
+invalidKeyPropertySpecs =
+  map
+    (assertParserFailure keyPropertyPairParser)
+    (invalidBoolProperties ++ invalidIntProperties)
+
+assertParserFailure
+  :: Show a => Parser a -> (String, ParseError ByteString Void) -> Spec
+assertParserFailure parser (input, expError) =
+  describe desc . works $
+    parse parser "" (fromString input) `shouldFailWith` expError
   where
-    assertParsesTo input = shouldParse . parse parser "" $ fromString input
-    descFun input expResult = "should parse " <> input <> " to " <> expResult
+    desc = "should parse on invalid " <> input
 
 spec :: Spec
-spec = sequence_ keyPropertyPairSpecs
+spec = sequence_ $ keyPropertyPairSpecs ++ invalidKeyPropertySpecs
