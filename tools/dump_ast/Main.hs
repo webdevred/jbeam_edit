f536997ebf790da4e89e0db402a5e799205091ad
debbie <148627186+webdevred@users.noreply.github.com>
2025-06-22 13:49:41 +0200

    migrate to use </> in dump_ast

--- tools/dump_ast/Main.hs
+++ tools/dump_ast/Main.hs
@@ -6,7 +6,8 @@ import Data.List (isSuffixOf)
 import Parsing.DSL (parseDSL)
 import Parsing.Jbeam (parseNodes)
 import System.Directory (getDirectoryContents)
-import Text.Pretty.Simple
+import System.FilePath ((</>))
+import Text.Pretty.Simple(pStringOpt, defaultOutputOptionsNoColor)
 
 import Data.ByteString.Lazy qualified as BL (
   readFile,
@@ -16,8 +17,8 @@ import Data.Text.Lazy.IO qualified as TLIO (writeFile)
 
 main :: IO ()
 main =
-  let jbflInputDir = "examples/jbfl"
-      jbeamInputDir = "examples/jbeam"
+  let jbflInputDir = "examples" </> "jbfl"
+      jbeamInputDir = "examples" </> "jbeam"
    in do
         jbeamFiles <-
           filter (isSuffixOf ".jbeam") <$> getDirectoryContents jbeamInputDir
@@ -31,24 +32,27 @@ saveDump outFile contents =
    in putStrLn ("creating " ++ outFile)
         >> TLIO.writeFile outFile formatted
 
+astDir :: FilePath
+astDir = "examples" </> "ast"
+
 dumpJbflAST :: String -> String -> IO ()
 dumpJbflAST dir filename = do
-  contents <- BL.readFile (dir ++ "/" ++ filename)
+  contents <- BL.readFile (dir </> filename)
   case parseDSL (BL.toStrict contents) of
     Right rs -> dump rs
     Left _ -> error $ "error " ++ filename
   where
     dump contents =
-      let outFile = "examples/ast/jbfl/" ++ takeWhile (/= '.') filename ++ ".hs"
+      let outFile = astDir </> "jbfl" </> takeWhile (/= '.') filename ++ ".hs"
        in saveDump outFile contents
 
 dumpJbeamAST :: String -> String -> IO ()
 dumpJbeamAST dir filename = do
-  contents <- BL.readFile (dir ++ "/" ++ filename)
+  contents <- BL.readFile (dir </> filename)
   case parseNodes (BL.toStrict contents) of
     Right rs -> dump rs
     Left _ -> error $ "error " ++ filename
   where
     dump contents =
-      let outFile = "examples/ast/jbeam/" ++ takeWhile (/= '.') filename ++ ".hs"
+      let outFile = astDir </> "jbeam" </> takeWhile (/= '.') filename ++ ".hs"
        in saveDump outFile contents
