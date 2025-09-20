c155e89f78713db228672171b7149ede21c60642
webdevred <148627186+webdevred@users.noreply.github.com>
2025-09-20 02:38:05 +0200

    Replaced org document with hadock comments

--- src/Core/Node.hs
+++ src/Core/Node.hs
@@ -25,6 +25,25 @@ data InternalComment = InternalComment
   }
   deriving (Eq, Ord, Read, Show)
 
+{- | Node type
+ Note that the concept of a `Node` here is different from "node" in jbeam, when I mention JBeam nodes in this document I will explicitly call them JBeam nodes or call them Vertices.
+In this document when I refer to nodes I mean a datatype I which I have created to represent arrays, objects and scalar inside a JBeam structure.
+type declares a type synomom for a already existing type while data declares a new type.
+So a Node can be:
+- a Array and contain a vector of Nodes
+- a Object and contain a vector of Nodes
+- a ObjectKey and contain a 2-element tuple (also called a pair) where the the first element is the key and the second is the value
+- a String which contains a Text
+- a Number which contains a Scientific, a numeric data type which I really like to due to it unlike most other types in many programming languages can express any arbitrary-precision decimal number without rounding errors or losing precision.
+- a Bool which contains either True or False
+- a MultilineComment or SinglelineComment which contains a Text
+- a Null meaning that the node is empty
+
+So in fact Node is a recursive data structure, a type which references itself.
+Text is yet a another string type in Haskell which is good fit when doing appends or comparisons which I tend to do quite a lot in this codebase.
+I comptemplated using something like [OMap](https://hackage.haskell.org/package/ordered-containers-0.2.4/docs/Data-Map-Ordered.html) for the Object but then I realized that I do not only need support pairs of keys and values for the Object case, I also need to support the Object having Comments as direct children.
+The parser is currently written using attoaparsec but I are considering to migrate to Megaparsec since Megaparsec has better support implementing error messages which can be given to the end user.
+-}
 data Node
   = Array Array
   | Object Object
