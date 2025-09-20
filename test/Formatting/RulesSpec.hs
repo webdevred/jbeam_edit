8945f96ec23d462d12a05475eb352368558ef00a
webdevred <148627186+webdevred@users.noreply.github.com>
2025-09-20 11:46:39 +0200

    Ran formatter

--- test/Formatting/RulesSpec.hs
+++ test/Formatting/RulesSpec.hs
@@ -8,18 +8,22 @@ import SpecHelper
 spec :: Spec
 spec = do
   describe "SomeKey & SomeProperty" $ do
-    it "Eq works for same PropertyKey" $ SomeKey PadAmount == SomeKey PadAmount `shouldBe` True
+    it "Eq works for same PropertyKey" $
+      SomeKey PadAmount == SomeKey PadAmount `shouldBe` True
 
-    it "Eq works for different PropertyKey" $ SomeKey PadAmount == SomeKey Indent `shouldBe` False
+    it "Eq works for different PropertyKey" $
+      SomeKey PadAmount == SomeKey Indent `shouldBe` False
 
     it "Show/Read roundtrip for SomeProperty" $ do
       let prop = SomeProperty PadAmount 5
       read (show prop) `shouldBe` prop
 
   describe "lookupKey" $ do
-    it "finds an existing key" $ lookupKey "PadAmount" allProperties `shouldBe` Just (SomeKey PadAmount)
+    it "finds an existing key" $
+      lookupKey "PadAmount" allProperties `shouldBe` Just (SomeKey PadAmount)
 
-    it "returns Nothing for missing key" $ lookupKey "NotAKey" allProperties `shouldBe` Nothing
+    it "returns Nothing for missing key" $
+      lookupKey "NotAKey" allProperties `shouldBe` Nothing
 
   describe "applyPadLogic" $ do
     let fakeNode = Number 123
@@ -28,4 +32,5 @@ spec = do
             [ (SomeKey PadAmount, SomeProperty PadAmount 7)
             , (SomeKey PadDecimals, SomeProperty PadDecimals 2)
             ]
-    it "applies PadAmount and PadDecimals" $ applyPadLogic formatScalarNode ruleSet fakeNode `shouldBe` " 123.00"
+    it "applies PadAmount and PadDecimals" $
+      applyPadLogic formatScalarNode ruleSet fakeNode `shouldBe` " 123.00"
