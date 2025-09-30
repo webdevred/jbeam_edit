da6a9db2de133d981f544a49d6ce9596c0060335
webdevred <148627186+webdevred@users.noreply.github.com>
2025-09-30 03:03:18 +0200

    Added app/.dir-locals.el

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-((haskell-mode . ((haskell-process-type . cabal-repl)))
- (eval . (setq-local haskell-process-args-cabal-repl
-                     (append '("--project-file" "cabal.project.dev") haskell-process-args-cabal-repl))))
