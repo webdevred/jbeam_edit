b518ba3ccf4cd68fa8a4f570409c5c306d4df666
webdevred <148627186+webdevred@users.noreply.github.com>
2025-10-01 02:27:46 +0200

    Fix and unify Emacs .dir-locals

--- app/.dir-locals.el
+++ app/.dir-locals.el
@@ -1,4 +1,4 @@
-(haskell-mode
+((haskell-mode
   . ((haskell-process-type . cabal-repl)
      (eval . (setq-local haskell-process-args-cabal-repl
-                     (append '("jbeam-edit:exe:jbeam-edit" "--project-file" "cabal.project.dev") haskell-process-args-cabal-repl))))))
+                         (append '("jbeam-edit:exe:jbeam-edit" "--project-file" "cabal.project.dev") haskell-process-args-cabal-repl))))))
--- src-extra/transformation/.dir-locals.el
+++ src-extra/transformation/.dir-locals.el
@@ -1,4 +1,4 @@
 ((haskell-mode
   . ((haskell-process-type . cabal-repl)
      (eval . (setq-local haskell-process-args-cabal-repl
-                     (append '("jbeam-edit:lib:jbeam-edit-transformation" "--project-file" "cabal.project.dev") haskell-process-args-cabal-repl))))))
+                         (append '("jbeam-edit:lib:jbeam-edit-transformation" "--project-file" "cabal.project.dev") haskell-process-args-cabal-repl))))))
