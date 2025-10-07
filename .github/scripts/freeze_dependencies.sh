70e02ed1477e6bf6939b96547218fd68a756d843
webdevred <148627186+webdevred@users.noreply.github.com>
2025-10-07 23:22:03 +0200

    Moved freeze dependency script to standalone script

---
+++ .github/scripts/freeze_dependencies.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cabal freeze --project-file "${CABAL_PROJECT:?CABAL_PROJECT missing}"
+PLAN_PATH=$(find dist-newstyle -name plan.json)
+JQ_QUERY='."install-plan" | map (."pkg-name") | unique'
+jq -c "$JQ_QUERY" < "$PLAN_PATH"
+printf "number of dependencies: %d" "$(jq -r "${JQ_QUERY}.[]" < "$PLAN_PATH" | wc -l)"
