57fbd9586b4cb14d1908b1bc3cfffc301ecc6fa6
August Johansson <148627186+webdevred@users.noreply.github.com>
2025-10-13 16:26:38 +0200

    Freeze index state (#83)
    * Freeze index state
    * Fix jq script

--- .github/scripts/freeze_dependencies.sh
+++ .github/scripts/freeze_dependencies.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 cabal freeze --project-file "${CABAL_PROJECT:?CABAL_PROJECT missing}"
 PLAN_PATH=$(find dist-newstyle -name plan.json)
-JQ_QUERY='."install-plan" | map (."pkg-name") | unique'
+JQ_QUERY='."install-plan" | map (."pkg-name" | select (. != "jbeam-edit")) | unique'
 jq -c "$JQ_QUERY" <"$PLAN_PATH"
 printf "number of dependencies: %d" "$(jq -r "${JQ_QUERY}.[]" <"$PLAN_PATH" | wc -l)"
