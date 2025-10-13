cd8a5be49e8a22d9934e464b7046231b0c664e35
webdevred <148627186+webdevred@users.noreply.github.com>
2025-10-13 15:59:24 +0200

    Emacs changes

--- editors/jbfl-mode.el
+++ editors/jbfl-mode.el
@@ -1,3 +1,27 @@
+;;; jbfl-mode.el --- Major mode for JBeam files -*- lexical-binding: t; -*-
+;;
+;; Author: August Johansson
+;; URL: https://github.com/webdevred/jbeam-edit
+;; Version: 0.1
+;; Keywords: languages
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;; BSD 3-Clause License, Copyright (c) 2025 webdevred
+;; All rights reserved.
+;; See https://opensource.org/licenses/BSD-3-Clause for full license text.
+;;
+;;; Commentary:
+;;
+;; This package provides a major mode `jbfl-mode` for editing jbfl configuration files used jbeam-edit.
+;;
+;; Features:
+;; - Syntax highlighting
+;; - Comment handling
+;;
+;;; Code:
+
 (defvar jbfl-mode-syntax-table
   (let ((st (make-syntax-table)))
     (modify-syntax-entry ?/ ". 124b" st)
@@ -20,9 +44,8 @@
 
     ("[{}]" . font-lock-builtin-face)
 
-    (";" . font-lock-builtin-face)
-    )
-  "Highlighting for `jbfl-mode`")
+    (";" . font-lock-builtin-face))
+  "Highlighting for `jbfl-mode`.")
 
 (define-derived-mode jbfl-mode prog-mode "Jbfl"
   "Jbfl mode."
@@ -31,6 +54,9 @@
               comment-start "//")
   (modify-syntax-entry ?: "." jbfl-mode-syntax-table))
 
+(when (featurep 'markdown-mode) (add-to-list 'markdown-code-lang-modes '("jbfl" . jbfl-mode)))
+
 (add-to-list 'auto-mode-alist '("\\.jbfl\\'" . jbfl-mode))
 
 (provide 'jbfl-mode)
+;;; jbfl-mode.el ends here
