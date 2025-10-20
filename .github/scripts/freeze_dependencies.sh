ade5e637a0bc84ee267fc40941ff4a543d748f8c
webdevred <148627186+webdevred@users.noreply.github.com>
2025-10-20 02:26:25 +0200

    Log version in freeze dependecny script

--- .github/scripts/freeze_dependencies.sh
+++ .github/scripts/freeze_dependencies.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 cabal freeze --project-file "${CABAL_PROJECT:?CABAL_PROJECT missing}"
 PLAN_PATH=$(find dist-newstyle -name plan.json)
-JQ_QUERY='."install-plan" | map (."pkg-name" | select (. != "jbeam-edit")) | unique'
+JQ_QUERY='."install-plan" | map (select (."pkg-name" != "jbeam-edit") | ."pkg-name" + "=" + ."pkg-version") | unique'
 jq -c "$JQ_QUERY" <"$PLAN_PATH"
 printf "number of dependencies: %d" "$(jq "${JQ_QUERY} | length" <"$PLAN_PATH")"
