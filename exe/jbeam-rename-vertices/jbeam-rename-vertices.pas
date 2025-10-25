0bc3ae3b9ce6f7b340774abf04da76470df0a27b
webdevred <148627186+webdevred@users.noreply.github.com>
2025-10-25 16:59:26 +0200

    tmp

--- exe/jbeam-rename-vertices/jbeam-rename-vertices.pas
+++ exe/jbeam-rename-vertices/jbeam-rename-vertices.pas
@@ -1,22 +1,24 @@
 program JbeamRenameVertices;
 
 uses
-  Windows, SysUtils;
+  Windows, SysUtils, ShellAPI;
 
 var
   hConsole: THandle;
-  csbi: TConsoleScreenBufferInfo;
   filePath, src, dst, answer, renameArgs: string;
   row: Integer;
 
 procedure ClrScr;
+var
+  csbi: TConsoleScreenBufferInfo;
+  charsWritten: DWORD;
 begin
   hConsole := GetStdHandle(STD_OUTPUT_HANDLE);
-  FillChar(csbi, SizeOf(csbi), 0);
   GetConsoleScreenBufferInfo(hConsole, csbi);
-  FillConsoleOutputCharacter(hConsole, ' ', csbi.dwSize.X * csbi.dwSize.Y, Windows.COORD(0,0), DWORD(csbi.dwCursorPosition.X));
-  FillConsoleOutputAttribute(hConsole, csbi.wAttributes, csbi.dwSize.X * csbi.dwSize.Y, Windows.COORD(0,0), DWORD(csbi.dwCursorPosition.X));
-  SetConsoleCursorPosition(hConsole, Windows.COORD(0,0));
+
+  FillConsoleOutputCharacter(hConsole, ' ', csbi.dwSize.X * csbi.dwSize.Y, COORD(0,0), charsWritten);
+  FillConsoleOutputAttribute(hConsole, csbi.wAttributes, csbi.dwSize.X * csbi.dwSize.Y, COORD(0,0), charsWritten);
+  SetConsoleCursorPosition(hConsole, COORD(0,0));
 end;
 
 procedure SetTextColor(color: Word);
@@ -40,9 +42,9 @@ begin
   row := 2;
 
   ClrScr;
-  SetTextColor(14); // gult
+  SetTextColor(14);
   GotoXY(10,1); WriteLn('==== JBeam Rename Vertices ====');
-  SetTextColor(7); // standard vit
+  SetTextColor(7); 
 
   GotoXY(5,row); Write('Enter file: '); ReadLn(filePath);
   Inc(row,2);
@@ -57,15 +59,16 @@ begin
     GotoXY(5,row); Write('Add more replacements? (y/n): '); ReadLn(answer); Inc(row,2);
   until LowerCase(answer) <> 'y';
 
-  SetTextColor(11); // cyan
+  SetTextColor(11);
   GotoXY(5,row); WriteLn('Command: jbeam-edit -i -n "', renameArgs, '" "', filePath, '"');
-  SetTextColor(7); Inc(row,2);
+  SetTextColor(7);
+  Inc(row,2);
 
   if ShellExecute(0, 'open', PChar('jbeam-edit'),
                   PChar('-i -n "' + renameArgs + '" "' + filePath + '"'),
                   nil, SW_SHOWNORMAL) <= 32 then
   begin
-    SetTextColor(12); // röd
+    SetTextColor(12);
     GotoXY(5,row); WriteLn('Could not run jbeam-edit. Is it in PATH?');
     SetTextColor(7);
     Inc(row,2);
