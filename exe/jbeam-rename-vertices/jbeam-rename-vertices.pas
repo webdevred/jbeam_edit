11a18af3c185c9800c5bfdca374d385bfe304d19
webdevred <148627186+webdevred@users.noreply.github.com>
2025-10-25 16:47:34 +0200

    tmp

--- exe/jbeam-rename-vertices/jbeam-rename-vertices.pas
+++ exe/jbeam-rename-vertices/jbeam-rename-vertices.pas
@@ -1,76 +1,76 @@
 program JbeamRenameVertices;
 
 uses
-WinCRT, SysUtils, Windows;
+  Windows, SysUtils;
 
 var
-   filePath, src, dst, answer, renameArgs: string;
-   row: integer;
+  hConsole: THandle;
+  csbi: TConsoleScreenBufferInfo;
+  filePath, src, dst, answer, renameArgs: string;
+  row: Integer;
 
-procedure PrintHeader;
+procedure ClrScr;
 begin
-   textcolor(yellow);
-   clrscr;
-   gotoxy(10,1);
-   writeln('===============================');
-   gotoxy(10,2);
-   writeln('    JBeam Rename Vertices      ');
-   gotoxy(10,3);
-   writeln('===============================');
-   textcolor(white);
+  hConsole := GetStdHandle(STD_OUTPUT_HANDLE);
+  FillChar(csbi, SizeOf(csbi), 0);
+  GetConsoleScreenBufferInfo(hConsole, csbi);
+  FillConsoleOutputCharacter(hConsole, ' ', csbi.dwSize.X * csbi.dwSize.Y, Windows.COORD(0,0), DWORD(csbi.dwCursorPosition.X));
+  FillConsoleOutputAttribute(hConsole, csbi.wAttributes, csbi.dwSize.X * csbi.dwSize.Y, Windows.COORD(0,0), DWORD(csbi.dwCursorPosition.X));
+  SetConsoleCursorPosition(hConsole, Windows.COORD(0,0));
+end;
+
+procedure SetTextColor(color: Word);
+begin
+  hConsole := GetStdHandle(STD_OUTPUT_HANDLE);
+  SetConsoleTextAttribute(hConsole, color);
+end;
+
+procedure GotoXY(x, y: Integer);
+var
+  coord: TCoord;
+begin
+  coord.X := x-1;
+  coord.Y := y-1;
+  SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 end;
 
 begin
-   SetConsoleOutputCP(CP_UTF8);
-   renameArgs := '';
-   row := 5;
+  SetConsoleOutputCP(CP_UTF8);
+  renameArgs := '';
+  row := 2;
 
-   PrintHeader;
+  ClrScr;
+  SetTextColor(14); // gult
+  GotoXY(10,1); WriteLn('==== JBeam Rename Vertices ====');
+  SetTextColor(7); // standard vit
 
-   gotoxy(5,row);
-   write('Enter file: ');
-   readln(filePath);
-   inc(row,2);
+  GotoXY(5,row); Write('Enter file: '); ReadLn(filePath);
+  Inc(row,2);
 
-   repeat
-      gotoxy(5,row);
-      write('Source (what to replace): ');
-      readln(src);
-      inc(row);
+  repeat
+    GotoXY(5,row); Write('Source (what to replace): '); ReadLn(src); Inc(row);
+    GotoXY(5,row); Write('Replacement (to what): '); ReadLn(dst); Inc(row);
 
-      gotoxy(5,row);
-      write('Replacement (to what): ');
-      readln(dst);
-      inc(row);
+    if renameArgs <> '' then renameArgs := renameArgs + ',';
+    renameArgs := renameArgs + src + ':' + dst;
 
-      if renameArgs <> '' then
-         renameArgs := renameArgs + ',';
-      renameArgs := renameArgs + src + ':' + dst;
+    GotoXY(5,row); Write('Add more replacements? (y/n): '); ReadLn(answer); Inc(row,2);
+  until LowerCase(answer) <> 'y';
 
-      gotoxy(5,row);
-      write('Add more replacements? (y/n): ');
-      readln(answer);
-      inc(row,2);
-   until LowerCase(answer) <> 'y';
+  SetTextColor(11); // cyan
+  GotoXY(5,row); WriteLn('Command: jbeam-edit -i -n "', renameArgs, '" "', filePath, '"');
+  SetTextColor(7); Inc(row,2);
 
-   textcolor(lightcyan);
-   gotoxy(5,row);
-   writeln('Command: jbeam-edit -i -n "', renameArgs, '" "', filePath, '"');
-   textcolor(white);
-   inc(row,2);
+  if ShellExecute(0, 'open', PChar('jbeam-edit'),
+                  PChar('-i -n "' + renameArgs + '" "' + filePath + '"'),
+                  nil, SW_SHOWNORMAL) <= 32 then
+  begin
+    SetTextColor(12); // röd
+    GotoXY(5,row); WriteLn('Could not run jbeam-edit. Is it in PATH?');
+    SetTextColor(7);
+    Inc(row,2);
+  end;
 
-   if ShellExecute(0, 'open', PChar('jbeam-edit'),
-                   PChar('-i -n "' + renameArgs + '" "' + filePath + '"'),
-                   nil, SW_SHOWNORMAL) <= 32 then
-   begin
-      textcolor(red);
-      gotoxy(5,row);
-      writeln('Could not run jbeam-edit. Is it in PATH?');
-      textcolor(white);
-      inc(row,2);
-   end;
-
-   gotoxy(5,row);
-   writeln('Done!');
-   readln;
+  GotoXY(5,row); WriteLn('Done!');
+  ReadLn;
 end.
