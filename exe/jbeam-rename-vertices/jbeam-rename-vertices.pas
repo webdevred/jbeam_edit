bcb70ef3ed33d40ac1bcabe257a846f5d407c76b
webdevred <148627186+webdevred@users.noreply.github.com>
2025-10-25 19:27:14 +0200

    tmp

--- exe/jbeam-rename-vertices/jbeam-rename-vertices.pas
+++ exe/jbeam-rename-vertices/jbeam-rename-vertices.pas
@@ -3,17 +3,7 @@ program JbeamRenameVertices;
 {$mode objfpc}{$H+}
 
 uses
-  Windows, SysUtils, Classes, IniFiles;
-
-function GetAppDataPath: string;
-var
-  buf: array[0..1023] of Char;
-begin
-  if Windows.GetEnvironmentVariable('APPDATA', buf, Length(buf)) <> 0 then
-    Result := StrPas(buf) + '\jbeam-edit\'
-  else
-    Result := '';
-end;
+  Windows, SysUtils, Classes;
 
 procedure RunJbeamEdit(const args: string);
 var
@@ -40,75 +30,40 @@ begin
 end;
 
 var
-  filePath, src, dst, renameArgs, answer, argStr: string;
-  replacements: TStringList;
-  ini: TIniFile;
-  alwaysBackup: Boolean;
-  iniPath: string;
+  filePath, src, dst, renameArgs: string;
 
 begin
   SetConsoleOutputCP(CP_UTF8);
 
-  iniPath := GetAppDataPath;
-  if iniPath = '' then
-  begin
-    Writeln('Could not find APPDATA path.');
-    Exit;
-  end;
-  ForceDirectories(iniPath);
-
-  ini := TIniFile.Create(iniPath + 'settings.ini');
-  alwaysBackup := ini.ReadBool('Settings', 'AlwaysBackup', True);
-  ini.Free;
-
   Write('Enter full path to .jbeam file: ');
   ReadLn(filePath);
-  if filePath = '' then Exit;
 
-  replacements := TStringList.Create;
-  try
-    renameArgs := '';
-    repeat
-      Write('Source (leave empty to finish): ');
-      ReadLn(src);
-      if src = '' then Break;
-      Write('Replacement: ');
-      ReadLn(dst);
-      if renameArgs <> '' then renameArgs := renameArgs + ',';
-      renameArgs := renameArgs + src + ':' + dst;
-    until False;
-
-    if renameArgs = '' then
-    begin
-      Writeln('No replacements entered.');
-      Exit;
-    end;
-
-    if alwaysBackup then
-      argStr := '-n "' + renameArgs + '" "' + filePath + '"'
-    else
-    begin
-      Write('Backup recommended. y=Backup, n=No Backup, c=Always No Backup: ');
-      ReadLn(answer);
-      case LowerCase(answer) of
-        'y': argStr := '-n "' + renameArgs + '" "' + filePath + '"';
-        'n': argStr := '-i -n "' + renameArgs + '" "' + filePath + '"';
-        'c':
-          begin
-            ini := TIniFile.Create(iniPath + 'settings.ini');
-            ini.WriteBool('Settings', 'AlwaysBackup', False);
-            ini.Free;
-            argStr := '-i -n "' + renameArgs + '" "' + filePath + '"';
-          end;
-      else
-        Writeln('Invalid option, using backup by default.');
-        argStr := '-n "' + renameArgs + '" "' + filePath + '"';
-      end;
-    end;
-
-    RunJbeamEdit(argStr);
-    Writeln('Done.');
-  finally
-    replacements.Free;
+  if filePath = '' then
+  begin
+    Writeln('No file selected.');
+    Exit;
   end;
+
+  renameArgs := '';
+
+  repeat
+    Write('Source (empty to finish): ');
+    ReadLn(src);
+    if src = '' then Break;
+
+    Write('Replacement: ');
+    ReadLn(dst);
+
+    if renameArgs <> '' then renameArgs := renameArgs + ',';
+    renameArgs := renameArgs + src + ':' + dst;
+  until False;
+
+  if renameArgs = '' then
+  begin
+    Writeln('No replacements entered.');
+    Exit;
+  end;
+
+  RunJbeamEdit('-n "' + renameArgs + '" "' + filePath + '"');
+  Writeln('Done.');
 end.
