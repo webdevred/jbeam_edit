422876b63c41ea2536df90075e1d7b62592de69f
webdevred <148627186+webdevred@users.noreply.github.com>
2025-11-14 17:38:01 +0100

    tmp

--- test-extra/language-server/WorkspaceLspSpec.hs
+++ test-extra/language-server/WorkspaceLspSpec.hs
@@ -4,32 +4,47 @@ import Control.Monad.IO.Class (liftIO)
 import Data.Text qualified as T (pack)
 import Language.LSP.Protocol.Types as LSP
 import Language.LSP.Test
+import System.Directory (getCurrentDirectory)
 import System.FilePath ((</>))
 import System.IO qualified as IO (readFile)
 import Test.Hspec
 
-exampleJbflFilepath :: FilePath
-exampleJbflFilepath = "examples" </> "ast" </> "jbfl" </> "minimal.hs"
+exampleJbflFilepath :: FilePath -> FilePath
+exampleJbflFilepath cwd =
+  cwd
+    </> "examples"
+    </> "jbfl"
+    </> "minimal.jbfl"
 
 spec :: Spec
-spec =
-  ( describe "JBeam LSP Formatter"
-      . it "formats a single JBeam file with a single JBFL rule correctly"
-  )
-    . runSession
-      ("jbeam-lsp-test-server " <> exampleJbflFilepath)
-      fullLatestClientCaps
-      "examples"
-    $ ( do
-          let jbeamFile = "jbeam" </> "fender.jbeam"
-              expectedFile = "examples" </> "formatted_jbeam" </> "fender-minimal-jbfl.jbeam"
+spec = do
+  cwd <- runIO getCurrentDirectory
+  workspaceSpec cwd
 
-          doc <- openDoc jbeamFile "jbeam"
+runJbeamSession :: FilePath -> Session a -> IO a
+runJbeamSession cwd =
+  runSession
+    ("jbeam-lsp-server -c " <> exampleJbflFilepath cwd)
+    fullLatestClientCaps
+    "examples"
 
-          formatDoc doc (LSP.FormattingOptions 0 False Nothing Nothing Nothing)
+formatVerify :: Session ()
+formatVerify = do
+  let jbeamFile = "jbeam" </> "fender.jbeam"
+      expectedFile = "examples" </> "formatted_jbeam" </> "fender-minimal-jbfl.jbeam"
 
-          formatted <- documentContents doc
-          expected <- liftIO (T.pack <$> IO.readFile expectedFile)
+  doc <- openDoc jbeamFile "jbeam"
 
-          liftIO $ formatted `shouldBe` expected
-      )
+  formatDoc doc (LSP.FormattingOptions 0 False Nothing Nothing Nothing)
+
+  formatted <- documentContents doc
+  expected <- liftIO (T.pack <$> IO.readFile expectedFile)
+
+  liftIO $ formatted `shouldBe` expected
+
+workspaceSpec :: FilePath -> Spec
+workspaceSpec cwd =
+  let label =
+        describe "JBeam LSP Formatter"
+          . it "formats a single JBeam file with a single JBFL rule correctly"
+   in label . runJbeamSession cwd $ formatVerify
