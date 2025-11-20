6e9c861277b796dee4fa73a0e3be8b38e18db038
webdevred <148627186+webdevred@users.noreply.github.com>
2025-11-20 14:20:34 +0100

    added formatting rules for glowMap padding

--- examples/ast/jbfl/minimal.hs
+++ examples/ast/jbfl/minimal.hs
@@ -5,6 +5,42 @@ RuleSet
                 ( fromList
                     [ AnyObjectKey
                     , Selector
+                        ( ObjectKey "glowMap" )
+                    , AnyObjectKey
+                    , Selector
+                        ( ObjectKey "off" )
+                    ]
+                )
+            , fromList
+                [
+                    ( SomeKey PadAmount
+                    , SomeProperty PadAmount 10
+                    )
+                ]
+            )
+        ,
+            ( NodePattern
+                ( fromList
+                    [ AnyObjectKey
+                    , Selector
+                        ( ObjectKey "glowMap" )
+                    , AnyObjectKey
+                    , Selector
+                        ( ObjectKey "on" )
+                    ]
+                )
+            , fromList
+                [
+                    ( SomeKey PadAmount
+                    , SomeProperty PadAmount 10
+                    )
+                ]
+            )
+        ,
+            ( NodePattern
+                ( fromList
+                    [ AnyObjectKey
+                    , Selector
                         ( ObjectKey "nodes" )
                     , AnyArrayIndex
                     , AnyArrayIndex
@@ -34,6 +70,176 @@ RuleSet
                 [
                     ( SomeKey NoComplexNewLine
                     , SomeProperty NoComplexNewLine True
+                    )
+                ]
+            )
+        ,
+            ( NodePattern
+                ( fromList
+                    [ AnyObjectKey
+                    , Selector
+                        ( ObjectKey "glowMap" )
+                    , Selector
+                        ( ObjectKey "abs" )
+                    ]
+                )
+            , fromList
+                [
+                    ( SomeKey PadAmount
+                    , SomeProperty PadAmount 20
+                    )
+                ]
+            )
+        ,
+            ( NodePattern
+                ( fromList
+                    [ AnyObjectKey
+                    , Selector
+                        ( ObjectKey "glowMap" )
+                    , Selector
+                        ( ObjectKey "battery" )
+                    ]
+                )
+            , fromList
+                [
+                    ( SomeKey PadAmount
+                    , SomeProperty PadAmount 20
+                    )
+                ]
+            )
+        ,
+            ( NodePattern
+                ( fromList
+                    [ AnyObjectKey
+                    , Selector
+                        ( ObjectKey "glowMap" )
+                    , Selector
+                        ( ObjectKey "checkengine" )
+                    ]
+                )
+            , fromList
+                [
+                    ( SomeKey PadAmount
+                    , SomeProperty PadAmount 20
+                    )
+                ]
+            )
+        ,
+            ( NodePattern
+                ( fromList
+                    [ AnyObjectKey
+                    , Selector
+                        ( ObjectKey "glowMap" )
+                    , Selector
+                        ( ObjectKey "hazard" )
+                    ]
+                )
+            , fromList
+                [
+                    ( SomeKey PadAmount
+                    , SomeProperty PadAmount 20
+                    )
+                ]
+            )
+        ,
+            ( NodePattern
+                ( fromList
+                    [ AnyObjectKey
+                    , Selector
+                        ( ObjectKey "glowMap" )
+                    , Selector
+                        ( ObjectKey "highbeam" )
+                    ]
+                )
+            , fromList
+                [
+                    ( SomeKey PadAmount
+                    , SomeProperty PadAmount 20
+                    )
+                ]
+            )
+        ,
+            ( NodePattern
+                ( fromList
+                    [ AnyObjectKey
+                    , Selector
+                        ( ObjectKey "glowMap" )
+                    , Selector
+                        ( ObjectKey "lowfuel" )
+                    ]
+                )
+            , fromList
+                [
+                    ( SomeKey PadAmount
+                    , SomeProperty PadAmount 20
+                    )
+                ]
+            )
+        ,
+            ( NodePattern
+                ( fromList
+                    [ AnyObjectKey
+                    , Selector
+                        ( ObjectKey "glowMap" )
+                    , Selector
+                        ( ObjectKey "lowpressure" )
+                    ]
+                )
+            , fromList
+                [
+                    ( SomeKey PadAmount
+                    , SomeProperty PadAmount 20
+                    )
+                ]
+            )
+        ,
+            ( NodePattern
+                ( fromList
+                    [ AnyObjectKey
+                    , Selector
+                        ( ObjectKey "glowMap" )
+                    , Selector
+                        ( ObjectKey "parkingbrake" )
+                    ]
+                )
+            , fromList
+                [
+                    ( SomeKey PadAmount
+                    , SomeProperty PadAmount 20
+                    )
+                ]
+            )
+        ,
+            ( NodePattern
+                ( fromList
+                    [ AnyObjectKey
+                    , Selector
+                        ( ObjectKey "glowMap" )
+                    , Selector
+                        ( ObjectKey "signal_L" )
+                    ]
+                )
+            , fromList
+                [
+                    ( SomeKey PadAmount
+                    , SomeProperty PadAmount 20
+                    )
+                ]
+            )
+        ,
+            ( NodePattern
+                ( fromList
+                    [ AnyObjectKey
+                    , Selector
+                        ( ObjectKey "glowMap" )
+                    , Selector
+                        ( ObjectKey "signal_R" )
+                    ]
+                )
+            , fromList
+                [
+                    ( SomeKey PadAmount
+                    , SomeProperty PadAmount 20
                     )
                 ]
             )
