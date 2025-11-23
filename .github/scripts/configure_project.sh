11425e766f310a74a5dd92f3a9a5755def4bc1ea
webdevred <148627186+webdevred@users.noreply.github.com>
2025-11-24 00:03:15 +0100

    Added benchmarks to configure script

--- .github/scripts/configure_project.sh
+++ .github/scripts/configure_project.sh
@@ -5,6 +5,11 @@ set -euo pipefail
 cabal configure --project-file cabal.project.release -O1
 
 printf "package jbeam-edit\n  tests: True\n" >>cabal.project.release.local
+
+if echo "$MATRIX_FLAGS" | grep -qa ' +transformation '; then
+  echo "  benchmarks: True" >>cabal.project.release.local
+fi
+
 if ! [[ "$MATRIX_FLAGS" == "" ]]; then
   echo "  flags: $MATRIX_FLAGS" >>cabal.project.release.local
 fi
