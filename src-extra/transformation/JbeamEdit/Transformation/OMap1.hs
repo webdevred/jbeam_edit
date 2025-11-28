476e75bf4eebd146806c7ab8d93dede1821c6dd1
webdevred <148627186+webdevred@users.noreply.github.com>
2025-11-28 20:20:00 +0100

    Added OMap1, will be good for type safe vertex tree operations

---
+++ src-extra/transformation/JbeamEdit/Transformation/OMap1.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns -Wno-incomplete-uni-patterns #-}
+
+module JbeamEdit.Transformation.OMap1 (OMap1, omap1Cons, omap1Uncons, omap1Snoc) where
+
+import Data.Map.Ordered (OMap)
+import Data.Map.Ordered qualified as OMap
+import GHC.IsList qualified as IsList
+import Text.Show qualified (Show (show))
+
+data OMap1 k v = OMap1 (k, v) (OMap k v) deriving stock (Eq)
+
+instance (Ord k, Show k, Show v) => Show (OMap1 k v) where
+  show om1 = "OMap1 " <> show (IsList.toList om1)
+
+instance Ord k => IsList (OMap1 k v) where
+  type Item (OMap1 k v) = (k, v)
+  toList (OMap1 (firstK, firstV) rest) = (firstK, firstV) : OMap.assocs rest
+  fromList ((firstK, firstV) : rest) = OMap1 (firstK, firstV) (OMap.fromList rest)
+
+omap1Uncons :: Ord k => OMap1 k v -> (k, v, OMap1 k v)
+omap1Uncons (OMap1 (firstK, firstV) rest) =
+  let Just newFirst@(newFirstK, _) = OMap.elemAt rest 0
+      newRest = OMap.delete newFirstK rest
+   in (firstK, firstV, OMap1 newFirst newRest)
+
+omap1Snoc :: Ord k => (k, v) -> OMap1 k v -> OMap1 k v
+omap1Snoc newFirst (OMap1 oldFirst rest) = OMap1 newFirst (oldFirst OMap.<| rest)
+
+omap1Cons :: Ord k => (k, v) -> OMap1 k v -> OMap1 k v
+omap1Cons newFirst (OMap1 oldFirst rest) = OMap1 oldFirst (rest OMap.>| newFirst)
