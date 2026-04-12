149dd65db036b2c4940e829e614385933a95f5d4
webdevred <148627186+webdevred@users.noreply.github.com>
2026-04-12 13:36:35 +0200

    Formatted new bash script

--- .github/scripts/check_stack_ghc_version.sh
+++ .github/scripts/check_stack_ghc_version.sh
@@ -6,8 +6,8 @@ CABAL_FILE="${CABAL_FILE:?Environment variable CABAL_FILE is required}"
 
 GHC=$(stack ghc -- --numeric-version)
 
-TESTED=$(awk -v field="tested-with" -f ./.github/script_helpers/get_cabal_field.awk "$CABAL_FILE" \
-  | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
+TESTED=$(awk -v field="tested-with" -f ./.github/script_helpers/get_cabal_field.awk "$CABAL_FILE" |
+  grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
 
 if ! echo "$TESTED" | grep -qxF "$GHC"; then
   echo "Stack GHC $GHC is not listed in tested-with."
