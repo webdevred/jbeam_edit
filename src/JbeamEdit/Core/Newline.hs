37078edbed219c0ddd7f6ce574f5e9f5977e9309
webdevred <148627186+webdevred@users.noreply.github.com>
2026-08-08 23:49:15 +0200

    Implemented newline detection support

---
+++ src/JbeamEdit/Core/Newline.hs
@@ -0,0 +1,14 @@
+module JbeamEdit.Core.Newline (detectNewline) where
+
+import Data.ByteString.Lazy as LBS
+import Data.ByteString.Lazy.Char8 as LBS8 (elemIndex)
+import System.IO (Newline (..))
+
+detectNewline :: LBS.ByteString -> Newline
+detectNewline content =
+  case LBS8.elemIndex '\r' content of
+    Nothing -> LF
+    Just idx ->
+      case LBS.index content (idx + 1) of
+        10 -> CRLF  -- '\n'
+        _  -> detectNewline (LBS.drop (idx + 1) content)
