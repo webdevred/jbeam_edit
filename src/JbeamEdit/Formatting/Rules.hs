2c6c62bfa3305053594ae6327b72116fa0e227bd
August Johansson <148627186+webdevred@users.noreply.github.com>
2026-08-20 19:27:32 +0200

    Upgraded containers to 0.8 (#223)

--- src/JbeamEdit/Formatting/Rules.hs
+++ src/JbeamEdit/Formatting/Rules.hs
@@ -283,7 +283,7 @@ sameBy matchMode f = go
        in res && go ps bs
     go ps bs = Seq.null ps && (Seq.null bs || PrefixMatch == matchMode)
 
--- TODO: when possible upgrade to containers 0.8 and migrate to M.filterKeys
+-- TODO: migrate to M.filterKeys once the stack snapshot ships containers 0.8
 findPropertiesForCursor :: MatchMode -> NC.NodeCursor -> RuleSet -> Rule
 findPropertiesForCursor matchMode cursor (RuleSet rs) =
   fold (M.filterWithKey (const . compareCursorAndPattern matchMode cursor) rs)
